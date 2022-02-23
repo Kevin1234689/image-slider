3ecea6f666777721c7a88b1e51f7cb8d5b442f8f
Kevin1234689 <100254080+Kevin1234689@users.noreply.github.com>
2022-02-22 19:59:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function calculate(){
+var cal=Number(document.getElementById("age_input").value);
+document.getElementById("answer").innerHTML="You are " + cal* 24* 365 + " hours old "
+}
---
+++ mian.js
@@ -0,0 +1,4 @@
+function next (){
+var images=["downtown Rome.jpg","romecity.jpg","rome.jpg"]
+document.getElementById("Rome").src=images[2] 
+} 
